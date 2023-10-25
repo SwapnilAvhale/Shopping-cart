ef53130e9602f0c3324b6b7b46ada72545b8ea8e
SwapnilAvhale <avhaleswapnil17@gmail.com>
2023-10-25 21:30:47 +0530

    first commit

---
+++ js/data.js
@@ -0,0 +1,74 @@
+let shopItemData = [{
+    id: 1,
+    name: "Casual Shirts",
+    price: 100,
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing.",
+    img: "images//img1.jpg"
+
+}, {
+    id: 2,
+    name: "Office Shirts",
+    price: 200,
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing.",
+    img: "images//img4.jpg"
+}, {
+    id: 3,
+    name: "T Shirts",
+    price: 50,
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing.",
+    img: "images//img3.jpg"
+}, {
+    id: 4,
+    name: "Mens Suits",
+    price: 300,
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing.",
+    img: "images//img2.jpg"
+}, {
+    id: 5,
+    name: "Mens Shoes",
+    price: 300,
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing.",
+    img: "images//img5.jpg"
+}, {
+    id: 6,
+    name: "Mens Jeans",
+    price: 200,
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing.",
+    img: "images//img6.jpg"
+}, {
+    id: 7,
+    name: "Mens watch",
+    price: 500,
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing.",
+    img: "images//img7.jpg"
+}, {
+    id: 8,
+    name: "Sneaker",
+    price: 400,
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing.",
+    img: "images//img8.jpg"
+}, {
+    id: 9,
+    name: "Dunke",
+    price: 900,
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing.",
+    img: "images//img9.jpg"
+}, {
+    id: 10,
+    name: "Mens Suits",
+    price: 300,
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing.",
+    img: "images//img2.jpg"
+}, {
+    id: 11,
+    name: "T Shirt",
+    price: 100,
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing.",
+    img: "images//img11.jpg"
+}, {
+    id: 12,
+    name: "Formal Shoes",
+    price: 800,
+    desc: "Lorem ipsum, dolor sit amet consectetur adipisicing.",
+    img: "images//img12.jpg"
+}]
